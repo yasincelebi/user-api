966c0cb0bea39a599f7e1a7a35442226d2fac904
yasincelebi <yasincelebix@gmail.com>
2021-05-07 03:38:20 +0300

    Add proptypes

---
+++ src/components/SingleUser/SingleUser.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './SingleUser.scss';
+const SingleUser = ({ ...props }) => {
+  return (
+    <div className="single_user_wrapper">
+      <div className="single_user_inwrapper">
+        <img src={props.picture} className="user_photo_thumbnail" />
+        <div className="user_job">
+          {/* iğrenç bi yöntem oldu */}
+          {props.job !== ''
+            ? props.job[(Math.random() * props.job.length - 1) | 1]
+            : 'Unemployed'}
+        </div>
+        <div className="user_fullname">
+          {props.name.first + ' ' + props.name.last}
+        </div>
+        <div className="user_state">
+          {props.location}, <span>{props.nat}</span>
+        </div>{' '}
+      </div>
+    </div>
+  );
+};
+
+export default SingleUser;
+SingleUser.propTypes = {
+  picture: PropTypes.string,
+  job: PropTypes.array,
+  name: PropTypes.object,
+  location: PropTypes.string,
+  nat: PropTypes.string,
+};
+SingleUser.defaultProps = {
+  picture: null,
+  job: null,
+  name: null,
+  location: null,
+};
