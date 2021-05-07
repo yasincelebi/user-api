8ed7196bdef5133fb1cb307827f48f26f53bf9eb
yasincelebi <yasincelebix@gmail.com>
2021-05-07 03:37:49 +0300

    Add sidebar

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import Users from '../Users/Users';
+import './MainPage.scss';
+const MainPage = () => {
+  return (
+    <>
+      <div className="main_wrapper">
+        <Sidebar />
+        <Users />
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
