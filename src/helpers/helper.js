ee10fbb78c01b244c082a18b749e41a4ca62c63f
yasincelebi <yasincelebix@gmail.com>
2021-05-08 02:36:47 +0300

    Add function comment

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,3 +1,8 @@
+/**
+ * Map function to keep the component file clean.
+ * @param {array} arr Array to map.
+ * @return {array} Mapped array.
+ */
 export const mapArr = (arr) => {
   let mappedArray = arr.map((e) => {
     return e;
