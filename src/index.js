671a873e0c19e8124f72d979f64cb3f7098037bf
yasincelebi <yasincelebix@gmail.com>
2021-05-08 06:12:18 +0300

    Add browserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,7 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { UserProvider } from './context/UserContext';
 ReactDOM.render(
   <UserProvider>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </UserProvider>,
   document.getElementById('root')
 );
